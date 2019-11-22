44b8d89a60e99d1ae1d9fc523596f81eea557039
nilsras <nils.rasamoel@gmail.com>
2019-11-22 17:30:00 +0100

    FixedUpdate instead of Update
    Changed VRController to use FixedUpdate for raycasting instead of Update. Plus made some minor changes on materials for gotogaze plane and waypoint, they're now rendered last

--- Assets/Scripts/VRController.cs
+++ Assets/Scripts/VRController.cs
@@ -37,7 +37,7 @@ public class VRController : MonoBehaviour
     }
 
     //Gets point where user is looking every frame and interacts with any intersecting gazeobjects if possible
-    void Update()
+    void FixedUpdate()
     {
         if (!_initialized) return;
         if (Input.GetKeyDown(KeyCode.H))
