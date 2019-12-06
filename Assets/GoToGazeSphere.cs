8c9c2579ecd20b320d7ba2fdbe17c1d4ed276f2d
nilsras <nils.rasamoel@gmail.com>
2019-12-06 16:10:13 +0100

    Speed Adjustement slider

--- Assets/GoToGazeSphere.cs
+++ Assets/GoToGazeSphere.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GoToGazeSphere : GazeObject {
-
+    [Header("GoToGaze Settings")]
     [SerializeField] private GameObject _wayPoint;
-    [SerializeField] private int _minNumOfPoints = 40;
-    [SerializeField] private float _verificationRadius = 10f;
     [SerializeField] private float _sphereRadius = 12.75f;
+    [SerializeField] [Range(0.0f, 1.0f)] private float _maxLinearVelocity;
     private List<Vector3> _gazePoints;
 
     [Header("Interface")] [SerializeField] private Vector2 _zeroOffset;
@@ -145,7 +144,7 @@ public class GoToGazeSphere : GazeObject {
         if (!IsActivated) return Vector2.zero;
 
         //If the focuspoint is higher than the middle of the sphere, ignore it
-        if (worldPos.y > transform.position.y) return Vector2.zero;
+        if (worldPos.y > transform.position.y - _sphereRadius/6f) return Vector2.zero;
 
         //Set the waypoint
         _wayPoint.transform.position = worldPos;
@@ -168,7 +167,7 @@ public class GoToGazeSphere : GazeObject {
         }
 
         //Calcualte the corresponding linear and angular speeds
-        controlResult.x = dist;
+        controlResult.x = (dist > _maxLinearVelocity)? _maxLinearVelocity : dist;
         controlResult.y = angle * (1f / 45);
 
         Debug.Log("G@G says: controlresult = " + controlResult);
@@ -194,130 +193,6 @@ public class GoToGazeSphere : GazeObject {
 
         if (isExtDisabled == true)
             this.OnUnhover();
-    }
-
-
-    public void UpdatePoint(RaycastHit hit)
-    {
-        //_wayPoint.transform.position = hit.point;
-
-        _gazePoints.Add(hit.point);
-
-        if (_gazePoints.Count > 1)
-        {
-
-            // Find max distance between points in list
-            Vector3[] list = findMaxDist(_gazePoints);
-
-            // If the 2 most distant points are within verification circle...
-            if (list[0].x <= 2 * _verificationRadius)
-            {
-                //float process = (float)_gazePoints.Count / _minNumOfPoints;
-
-                // Update Selection Radial - OBS: THIS IS FRAME-UPDATE DEPENDENT, MEANING THAT THE G2G RADIAL WILL FILL IN DIFFERENT TEMPOS DEPENDENT ON THE PC RUNNING THE GAME. 
-                // A BETTER SOLUTION IS TO USE selectionRadial.HandleOver() & selectionRadial.HandleOut() and wait the OnSelectionComplete-event, as with VRInteractiveItems.
-                // No time to implement this solution, but should definitely be implemented in future developments. It shouldn't be too complicated.
-                //selectionRadial.Show();
-                //selectionRadial.FillSelectionRadial(process);
-
-                if (_gazePoints.Count >= _minNumOfPoints)
-                {
-                    Vector3 centerPoint = ((list[2] - list[1]) / 2) + list[1];  // Center point between 2 most distant points
-                    //Debug.Log("Point verified. Center: "+centerPoint);
-                    //agent.updateRotation = true;
-                    //agent.SetDestination(hit.point);
-
-                    //Set the waypoint
-                    _wayPoint.transform.position = centerPoint;
-                    _wayPoint.SetActive(true);
-                    
-                    _gazePoints.Clear();
-                    _gazePoints.TrimExcess();
-                }
-            }
-            else
-            {
-                // Clear Selection Radial
-                //selectionRadial.FillSelectionRadial(0f);
-                //selectionRadial.Hide();
-
-                //Hide waypoint
-                //_wayPoint.SetActive(false);
-
-                // Clear List
-                _gazePoints.Clear();
-                _gazePoints.TrimExcess();
-            }
-        }
-
-        /*if (agent.hasPath)
-        {
-            targetMark.GetComponent<MeshRenderer>().enabled = true;
-            targetMark.transform.position = agent.pathEndPosition;
-            targetMark.transform.localScale.Set(verificationRadius, 0.01f, verificationRadius);
-        }
-        else targetMark.GetComponent<MeshRenderer>().enabled = false;*/
-    }
-
-    /**Taken from Jacopto's GoToGazeInterface
-     * Finds the longest distance in a list of vectors
-     * 
-     */
-    Vector3[] findMaxDist(List<Vector3> list)
-    {
-        float temp;
-        float max = 0f;
-        Vector3[] distantPoints = new Vector3[3];
-        for (int i = 0; i < list.Count; i++)
-        {
-            for (int j = 0; j < list.Count; j++)
-            {
-                if (i != j)
-                {
-                    temp = Vector3.Distance(list[i], list[j]); // Distance between points
-
-                    if (temp > max)
-                    {
-                        max = temp;
-                        distantPoints[1] = list[i];
-                        distantPoints[2] = list[j];
-                    }
-                }
-            }
-        }
-        distantPoints[0] = new Vector3(max, max, max);
-        return distantPoints;
-    }
-
-    /// <summary>
-    /// Calculates the correct output depending on gazepoint on the trackpad.
-    /// </summary>
-    public Vector2 GetControlResult()
-    {
-        if (!_wayPoint.activeSelf) return Vector2.zero;
-        Vector2 controlResult = new Vector2();
-        Vector2 dirVector = new Vector2(_wayPoint.transform.position.x, _wayPoint.transform.position.z);
-        float angle = Vector2.SignedAngle(Vector2.up, dirVector);
-        float dist = SphericalDistance(
-            transform.position - Vector3.up * _sphereRadius, 
-            _wayPoint.transform.position
-            ) * (_sphereRadius / 3.5f);
-        //Debug.Log("G2G says: angle = " + angle + ", dist = " + dist);
-
-        //If the distance is too low, ignore and send a zero value
-        if (dist < 0.1)
-        {
-            _wayPoint.SetActive(false);
-            return Vector2.zero;
-        }
-
-        //Calcualte the corresponding linear and angular speeds
-        controlResult.x = dist;
-        controlResult.y = angle * (1f / 45);
-
-        Debug.Log("G@G says: controlresult = " + controlResult);
-
-        return controlResult;
     }
 
     /// <summary>
